5ebfabb35f07dc255e64b1df7c68cdf52ea3c178
Xiaomei Du <uiaoei@gmail.com>
2022-11-30 16:13:21 +0800

    Added index page

--- pages/pets/index.js
+++ pages/pets/index.js
@@ -1,4 +1,5 @@
 // pages/pets/index.js
+const app = getApp();
 import event from '@codesmiths/event';
 Page({
 
@@ -7,6 +8,14 @@ Page({
      */
     data: {
 
+    },
+
+    goToPetDetail(e) {
+        console.log('goToPetDetail', e)
+        const id =  e.currentTarget.dataset.id
+        wx.navigateTo({
+          url: '/pages/pets/show',
+        })
     },
 
     /**
@@ -47,8 +56,21 @@ Page({
     /**
      * Lifecycle function--Called when page show
      */
+    
     onShow() {
+      const page = this;
+      wx.request({
+        url: "http://127.0.0.1:3000/api/v1/pets",
+        method: 'GET',
+        success(res) {
+            console.log("pets", res.data.pets)
+          const pets = res.data.pets;
 
+          page.setData ({
+            pets: pets
+          });
+        }
+      })
     },
 
     /**
