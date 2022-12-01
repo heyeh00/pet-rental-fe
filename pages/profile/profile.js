e898ccb8291c79e1e1b54315d9b17cef97978ef9
Henry Yeh <heyeh@icloud.com>
2022-12-01 14:00:46 +0800

    going to restart this process in profile-page2 branch

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -8,17 +8,13 @@ Page({
      * Page initial data
      */
     data: {
-        userInfo: {},
-        hasUserInfo: false,
-        canIUse: wx.canIUse('button.open-type.getUserInfo'),
-        canIUseGetUserProfile: false,
-        canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
     },
 
     /**
      * Lifecycle function--Called when page load
      */
     onLoad(options) {
+
         if (getApp().globalData.header) {
             this.getUser();
         } else {
@@ -29,27 +25,53 @@ Page({
             this.setData({
               canIUseGetUserProfile: true
             })
-          }
+        }
     },
 
-    getUserBookings(userId) {
-        wx.request({
-          url: `http://localhost:3000/api/v1/users/${userId}/bookings`,
-          success(res) {
-              console.log("BOOKING", res)
-          },
-          failure(errors) {
-              console.log("ERROR", errors)
-          }
-        })
+    login(e) {
+        const that = this
+        wx.getUserProfile({
+            desc: 'desc the user to himself',
+            success(res) {
+                console.log("GET USER NAME", res.userInfo.nickName)
+                that.setData({name: res.userInfo.nickName})
+                app.globalData.user.name = that.data.name
+
+                console.log("GET USER IMAGE", res.userInfo.avatarUrl)
+                that.setData({image_url: res.userInfo.avatarUrl})
+                app.globalData.user.image_url = that.data.image_url
+
+                const checkData = {
+                    name: that.data.name,
+                    image_url: that.data.image_url,
+                }
+                console.log("===CHECK DATA===", checkData)
+
+                wx.request({
+                  header: app.globalData.header,
+                  url: `http://localhost:3000/api/v1/users/${app.globalData.user.id}`,
+                  method: "POST",
+                  data: {
+                      name: that.data.name,
+                      image_url: that.data.image_url
+                  },
+                  success(res) {
+                      console.log("PLEASE WORK", res)
+                  }
+                })
+            },
+            fail(errors) {
+                console.log("ERRORS", errors)
+            }
+          })
     },
 
     getUser() {
         this.setData({ user: app.globalData.user })
         this.setData({ user: this.data.user })
-        console.log('==USER DATA==', this.data.user);
         const userId = this.data.user.id;
-        
+        console.log('==USER ID==', userId);
+        console.log('==CODE==', app.globalData.code)
     },
 
     /**
