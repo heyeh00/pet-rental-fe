c9877a801fc5ffeafb31f0c77877345562aa7c14
Henry Yeh <heyeh@icloud.com>
2022-12-02 01:18:15 +0800

    finished create form

--- pages/pets/create.js
+++ pages/pets/create.js
@@ -1,3 +1,5 @@
+const { getData } = require("../../utils/getdata")
+const app = getApp()
 // pages/pets/create.js
 Page({
 
@@ -8,6 +10,23 @@ Page({
 
     },
 
+    submitRestaurant(e) {
+      console.log(e.detail.value)
+      const {name, breed, animal, age, personality, address, gender} = e.detail.value
+      const user_id = app.getUserId()
+      this.setData({pet: {name, breed, animal, age, personality, address, gender, user_id}})
+      const pet = this.data.pet
+      console.log("DATA TO POST", pet)
+      getData(`/users/${user_id}/pets`, { pet } , "POST").then((res) => {
+        const id = res.data.pet.id
+        wx.navigateTo({
+          url: '/pages/pets/show',
+          success: function(res) {
+            res.eventChannel.emit('acceptDataFromOpenerPage', { id }) 
+          }
+        })
+      })
+    },
     /**
      * Lifecycle function--Called when page load
      */
